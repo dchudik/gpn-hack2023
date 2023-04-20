ec8a358d539f1f5f644881d84001682ee666416c
Dima Chudinov <d.chudinov.spb@yandex.ru>
2023-04-20 22:41:42 +0300

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://dchudik.github.io/gpn-hack2023/",
   plugins: [react()],
-})
+});
